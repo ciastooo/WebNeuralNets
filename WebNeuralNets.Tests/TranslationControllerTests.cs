e696777af88a7317f933183bc6984b7912ef9a61
Piotr Wontka <leene4@gmail.com>
2018-12-01 12:45:54 +0100

    Unit tests for translations

---
+++ WebNeuralNets.Tests/TranslationControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using WebNeuralNets.Controllers;
+using NUnit.Framework;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using WebNeuralNets.BusinessLogic;
+using Moq;
+using WebNeuralNets.Models.Enums;
+
+namespace GroupProjectBackend.Tests
+{
+    [TestFixture]
+    public class TranslationControllerTests
+    {
+        private readonly ITranslationHelper _translationHelper;
+
+        public TranslationControllerTests()
+        {
+            var translationHelper = new Mock<ITranslationHelper>();
+            translationHelper.Setup(x => x.GetTranslation(It.IsAny<LanguageCode>(), It.IsAny<string>())).Returns("NOTFOUND");
+            translationHelper.Setup(x => x.GetTranslation(LanguageCode.ENG, "ENGtest")).Returns("English testing");
+            translationHelper.Setup(x => x.GetTranslation(LanguageCode.PL, "PLtest")).Returns("Polskie testowanie");
+            _translationHelper = translationHelper.Object;
+        }
+
+        [Test]
+        [TestCase(LanguageCode.ENG, "ENGtest", "English testing")]
+        [TestCase(LanguageCode.PL, "ENGtest", "NOTFOUND")]
+        [TestCase(LanguageCode.PL, "PLtest", "Polskie testowanie")]
+        [TestCase(LanguageCode.ENG, "PLtest", "NOTFOUND")]
+        [TestCase(LanguageCode.PL, "XXX", "NOTFOUND")]
+        [TestCase(LanguageCode.ENG, "XXX", "NOTFOUND")]
+        public void GetTranslation_WhenLanguageCodeAndKeyIsPassed_ShouldReturnExpectedValue(LanguageCode languageCode, string key, string expectedResult)
+        {
+            // Arrange
+            var controller = new TranslationController(_translationHelper);
+
+            // Act
+            var response = controller.GetTranslation(key, languageCode) as ObjectResult;
+
+            // Assert
+            response.Should().NotBeNull();
+            response.Should().BeOfType<OkObjectResult>();
+            response.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            response.Value.Should().Be(expectedResult);
+        }
+    }
+}
